2924086c61f150cbe6996f6f166c1a8bd8b28c14
Zohan <zirt1245@gmail.com>
2026-04-05 18:33:42 +0300

    add color terminal

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use colored::Colorize;
 use libc::{AF_PACKET, ETH_P_ALL, SOCK_RAW, if_nametoindex, recvfrom, socket};
 use promiscuous_mode::enable_promiscuous;
 use std::{
@@ -12,19 +13,19 @@ use std::{
 #[command(name = "raw-sniffer", about = "Raw packet sniffer")]
 struct Cli {
     /// Network interface to listen on (e.g. eth0, wlp0s20f3)
-    interface: String, // сетевой интефейс
+    interface: String,
 
     /// Filter by protocol: tcp, udp, icmp
     #[arg(long)]
-    proto: Option<String>, // фильтр протокола
+    proto: Option<String>,
 
     /// Filter by port number
     #[arg(long)]
-    port: Option<u16>, // фильтр порта
+    port: Option<u16>,
 
     /// Filter by IP address
     #[arg(long)]
-    ip: Option<String>, // фильтр ip
+    ip: Option<String>,
 }
 
 mod promiscuous_mode;
@@ -64,24 +65,57 @@ fn parse_ethernet(
                 let ip_header = &buf[14..];
                 let proto_code = ip_header[9];
 
+                let ihl = ip_header[0] & 0x0F;
+                let ip_header_len = (ihl * 4) as usize;
+
+                let transport = &ip_header[ip_header_len..];
+
                 let proto_name = match proto_code {
-                    6 => String::from("TCP"),
-                    17 => String::from("UDP"),
-                    1 => String::from("ICMP"),
-                    2 => String::from("IGMP"),
+                    6 => {
+                        if transport.len() < 14 {
+                            return println!("TCP (invalid)");
+                        }
+
+                        let flags = transport[13];
+
+                        let syn = flags & 0b00000010 != 0;
+                        let ack = flags & 0b00010000 != 0;
+                        let fin = flags & 0b00000001 != 0;
+                        let rst = flags & 0b00000100 != 0;
+
+                        let mut flag_list = Vec::new();
+
+                        if syn { flag_list.push("SYN".green().to_string()); }
+                        if ack { flag_list.push("ACK".yellow().to_string()); }
+                        if fin { flag_list.push("FIN".red().to_string()); }
+                        if rst { flag_list.push("RST".red().to_string()); }
+                        /* SYN: первый пакет, хочу подключится
+                        ACK: получил пакет, установка соединения
+                        FIN: все пакеты отправили, закрытие соединения
+                        RST: ошибка покета */
+                        
+                        if flag_list.is_empty() {
+                            format!("TCP: no flags")
+                        } else {
+                            format!("TCP: {}", flag_list.join(", "))
+                        }
+                    },
+                    17 => "UDP".blue().to_string(),
+                    1 => "ICMP".cyan().to_string(),
+                    2 => "IGMP".purple().to_string(),
                     _ => String::from("Other IP protocol"),
                 };
 
                 if let Some(filter) = proto_filter {
-                    if filter.to_uppercase() != proto_name {
+                    if !proto_name.to_uppercase().starts_with(&filter.to_uppercase()) {
                         return;
                     }
                 }
 
-                let ipv4_info = parse_ipv4(ip_header);
+                let (ipv4_info, src_port, dst_port) = parse_ipv4(ip_header);
 
                 if let Some(port) = port_filter {
-                    if !ipv4_info.contains(&format!(":{}", port)) {
+                    if src_port != Some(*port) && dst_port != Some(*port) {
                         return;
                     }
                 }
@@ -110,15 +144,15 @@ fn parse_ethernet(
             } // ищет MAC какого-то IP
             _ => String::from("Неизвестный EtherType"),
             // базовые вложенные протоколы
-        };
+        }; 
 
         println!("{} | {}", str_dst_src, ethertype_name);
     }
 }
 
-fn parse_ipv4(buf: &[u8]) -> String {
+fn parse_ipv4(buf: &[u8]) -> (String, Option<u16>, Option<u16>) {
     if buf.len() < 20 {
-        return String::from("Invalid IPv4");
+        return (String::from("Invalid IPv4"), None, None);
     }
 
     let protocol = buf[9];
@@ -129,22 +163,31 @@ fn parse_ipv4(buf: &[u8]) -> String {
     let src = format!("{}.{}.{}.{}", buf[12], buf[13], buf[14], buf[15]);
     let dst = format!("{}.{}.{}.{}", buf[16], buf[17], buf[18], buf[19]);
 
-    let mut ports_scr = String::new();
-    let mut ports_dst = String::new();
+    let mut ports_src_str = String::new();
+    let mut ports_dst_str = String::new();
+    let mut src_port: Option<u16> = None;
+    let mut dst_port: Option<u16> = None;
 
     if protocol == 6 || protocol == 17 {
         let transport = &buf[ip_header_len..];
 
         if transport.len() >= 4 {
-            let src_port = u16::from_be_bytes([transport[0], transport[1]]);
-            let dst_port = u16::from_be_bytes([transport[2], transport[3]]);
+            let sp = u16::from_be_bytes([transport[0], transport[1]]);
+            let dp = u16::from_be_bytes([transport[2], transport[3]]);
 
-            ports_scr = format!(":{}", src_port);
-            ports_dst = format!(":{}", dst_port);
+            src_port = Some(sp);
+            dst_port = Some(dp);
+
+            ports_src_str = format!(":{}", sp);
+            ports_dst_str = format!(":{}", dp);
         }
     }
 
-    format!("IPv4: {}{} -> {}{}", src, ports_scr, dst, ports_dst)
+    (
+        format!("IPv4: {}{} -> {}{}", src, ports_src_str, dst, ports_dst_str),
+        src_port,
+        dst_port,
+    )
 }
 
 fn main() {
